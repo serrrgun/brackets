1306633c4405c11c8dd50eff55cf1ebb926e8f54
serrrgun <serrrgun@mail.ru>
2020-03-18 20:39:11 +0300

    feat: adds solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    for (let i = 0; i < bracketsConfig.length;) {
+        const configItem = bracketsConfig[i].join('')
+        
+        if (str.includes(configItem)) {
+            str = str.replace(configItem, '')
+            i = 0
+        } else {
+            i++
+        }
+    };
+    
+    return str.length == 0 ? true : false;
 }
